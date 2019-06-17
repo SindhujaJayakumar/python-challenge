4ee974252de1d87ec039259263fc11dca9651dcf
Sindhu Singaravelan <fromsindhu@gmail.com>
2019-06-17 16:47:14 -0500

    Output csv created

---
+++ PyBank/main.py
@@ -0,0 +1,81 @@
+import os
+import csv
+
+csvpath = os.path.join("C:/Bootclassactivities/SMUDAL201905DATA5/02-Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv")
+
+profit = 0
+loss = 0
+amount = 0
+total = 0
+months = 0
+
+prev_month = 0
+inc_dec = 0
+great_inc = 0
+great_dec = 0
+sum_inc_dec = 0
+average = 0
+index = 0
+
+with open(csvpath, newline="") as csvfile:
+   csv_reader = csv.reader(csvfile, delimiter=",")
+
+   csv_header = next(csvfile)
+
+   for row in csv_reader:
+       amount = float(row[1])
+       #profit_list.append(amount)
+       if amount > 0:
+           profit = profit + amount
+       else:
+           loss = loss + amount
+
+       inc_dec = amount - prev_month
+       if index != 0:
+           sum_inc_dec = sum_inc_dec + inc_dec
+
+       if (inc_dec > 0) and (inc_dec > great_inc):
+           great_inc = inc_dec
+           month_row_inc = row[0]
+       elif (inc_dec < 0) and (inc_dec < great_dec):
+           great_dec = inc_dec
+           month_row_dec = row[0]
+
+       prev_month = amount
+       index +=1
+
+   total = profit + loss
+
+   months = len(list(csv.reader(open(csvpath))))
+   # Check how many months are in the file not considering the header
+   months = months - 1
+
+   # Calculate the average dividing the total of changes by the number of changes
+   average = sum_inc_dec / (months - 1)
+
+   print(f"Financial Analysis")
+   print(f"----------------------------")
+   print(f"Total Months: {months}")
+   print(f"Total: $ {total}")
+   print(f"Average Change: $ {average}")
+   print(f"Greatest Increase in Profits: {month_row_inc} ($ {great_inc})")
+   print(f"Greatest Decrease in Profits: {month_row_dec} ($ {great_dec})")
+
+output_path = os.path.join ("Financial_Analysis.csv")
+
+# Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+   # Initialize csv.writer
+   csvwriter = csv.writer(csvfile)
+
+   # Write the second row
+#    csvwriter.writerow([""])
+
+   csvwriter.writerow(["Financial Analysis"])
+   csvwriter.writerow(["----------------------------"])
+   csvwriter.writerow([f"Total Months: {months}"])
+   csvwriter.writerow([f"Total: $ {total}"])
+   csvwriter.writerow([f"Average Change: $ {average}"])
+   csvwriter.writerow([f"Greatest Increase in Profits: {month_row_inc} ($ {great_inc})"])
+   csvwriter.writerow([f"Greatest Decrease in Profits: {month_row_dec} ($ {great_dec})"])
---
+++ PyPoll/main.py
@@ -0,0 +1,88 @@
+import os
+import csv
+
+csvpath = os.path.join("C:/Bootclassactivities/SMUDAL201905DATA5/02-Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv")
+
+candidate = []
+votes = []
+total_votes = 0
+count = 0
+khan = 0
+total_candidates_with_votes = 0
+i = 0
+votes_candidate = 0
+percent = 0
+winner_percent = 0
+
+with open(csvpath, newline="") as csvfile:
+   csv_reader = csv.reader(csvfile, delimiter=",")
+
+   csv_header = next(csvfile)
+
+   total_votes  = len(list(csv.reader(open(csvpath))))
+
+   for row in csv_reader:
+       name = row[2]
+       votes.append(name)
+       if name in candidate:
+           continue
+       else:
+           candidate.append(name)
+
+   print(f"Election Results")
+   print(f"----------------------------")
+   print(f"Total Votes: {total_votes}")
+   print(f"----------------------------")
+
+   total_candidates_with_votes = len(candidate)
+
+   while i < total_candidates_with_votes:
+       name_aux = candidate[i]
+       votes_candidate = votes.count(name_aux)
+       percent = round((votes_candidate / total_votes * 100),2)
+       
+       if percent > winner_percent:
+           winner_percent = percent
+           winner_name = name_aux
+
+       print(f"{name_aux}: {percent}% ({votes_candidate})")
+       i = i + 1
+
+   print(f"----------------------------")
+   print(f"Winner: {winner_name}")
+   print(f"----------------------------")
+
+output_path = os.path.join ("Election_Results.csv")
+
+   # Open the file using "write" mode. Specify the variable to hold the contents
+with open(output_path, 'w', newline='') as csvfile:
+
+   # Initialize csv.writer
+   csvwriter = csv.writer(csvfile)
+
+   # Write the second row
+   #csvwriter.writerow([""])
+
+   csvwriter.writerow(["Election Results"])
+   csvwriter.writerow(["--------------------------------------"])
+   csvwriter.writerow([f"Total Votes: {total_votes}"])
+   csvwriter.writerow(["--------------------------------------"])
+   
+   #Not sure how to write out each candidate's results
+   total_candidates_with_votes = len(candidate)
+
+   while i < total_candidates_with_votes:
+       name_aux = candidate[i]
+       votes_candidate = votes.count(name_aux)
+       percent = round((votes_candidate / total_votes * 100),2)
+       
+       if percent > winner_percent:
+           winner_percent = percent
+           winner_name = name_aux
+
+       csvwriter.writerow([f"{name_aux}: {percent}% ({votes_candidate})"])
+       i = i + 1
+
+   csvwriter.writerow(["--------------------------------------"])
+   csvwriter.writerow([f"Winner: {winner_name}"])
+   csvwriter.writerow(["--------------------------------------"])   
